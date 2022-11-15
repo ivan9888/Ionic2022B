e14919e9c5bb418c88c51a9bb2e811aa0fa5cdc0
Ivan Andrade <ivanandrade.98@hotmail.com>
2022-11-14 20:33:12 -0600

    Practica4

--- Practica01/src/main.js
+++ Practica01/src/main.js
@@ -23,9 +23,34 @@ import '@ionic/vue/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////// FIREBASE
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCQLDXiURpOeeo7F0HPSw-1gqXIuW1IwL8",
+  authDomain: "practica3-8fd54.firebaseapp.com",
+  databaseURL: "https://practica3-8fd54-default-rtdb.firebaseio.com",
+  projectId: "practica3-8fd54",
+  storageBucket: "practica3-8fd54.appspot.com",
+  messagingSenderId: "746058520079",
+  appId: "1:746058520079:web:d7fd0e0ab1986e3f59f57a"
+};
+
+// Initialize Firebase
+const appFirebase = initializeApp(firebaseConfig);
+
+
+
 const app = createApp(App)
   .use(IonicVue)
-  .use(router);
+  .use(router)
+  .use(appFirebase);
   
 router.isReady().then(() => {
   app.mount('#app');
